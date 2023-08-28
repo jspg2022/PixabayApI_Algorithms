491926da9ab459edaf80ad0c3f9ae5b87ffbef0b
reut <116121073+jspg2022@users.noreply.github.com>
2023-08-28 23:31:17 +0300

    Add files via upload

--- _7_decodeBits.js
+++ _7_decodeBits.js
@@ -1,3 +1,34 @@
+const decodeBits1 = function (bits) {
+  const arr = bits.split(1).filter(Boolean);
+  const max = Math.max(...arr.map((el) => el.length));
+  const min = Math.min(...arr.map((el) => el.length));
+  const n = max / 7;
+
+  const zero = '0';
+  const one = '1';
+
+  bitsObj = {
+    '.': one.repeat(n), //   "Dot" – is 1 time unit long // decode "1"*n
+    '-': one.repeat(n * 3), //   "Dash" – is 3 time units long.
+    '': zero.repeat(n), //   Pause between dots and dashes in a character – is 1 time unit long.
+    ' ': zero.repeat(n * 3), //   Pause between characters inside a word – is 3 time units long.
+    '   ': zero.repeat(n * 7), //   Pause between words – is 7 time units long.
+  };
+
+  const bitsArr = Object.values(bitsObj);
+  sortBitsArr = bitsArr.sort((a, b) => b.length - a.length);
+
+  let morseCode = bits;
+  let currentBits;
+
+  for (let i = 0; i < sortBitsArr.length; i++) {
+    currentBits = sortBitsArr[i];
+    sign = Object.keys(bitsObj).find((key) => bitsObj[key] === currentBits);
+    morseCode = morseCode.split(currentBits).join(sign);
+  }
+  return morseCode;
+};
+
 const decodeBits = function (bits) {
   const arr = bits.split(1).filter(Boolean);
   const max = Math.max(...arr.map((el) => el.length));
@@ -121,4 +152,8 @@ const decodeMorse = function (morseInput) {
 const bits =
   '1100110011001100000011000000111111001100111111001111110000000000000011001111110011111100111111000000110011001111110000001111110011001100000011';
 
+console.log(decodeBits1(bits));
+console.log(decodeBits(bits));
+
+decodeMorse(decodeBits1(bits));
 decodeMorse(decodeBits(bits));
